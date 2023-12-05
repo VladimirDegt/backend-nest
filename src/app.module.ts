a3b8357dc263445aa93d98daa7e732f9104ad8e9
Volodymyr <degtyarevvladimirr@gmail.com>
2023-12-05 19:41:03 +0200

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
         CatsModule,
-        MongooseModule.forRoot(
-            'mongodb+srv://vladimirr:E0b2mNwKL1bG77j1@cluster0.ogz1o8j.mongodb.net/',
-        ),
-    ], 
+        MongooseModule.forRoot(process.env.DB_URL),
+    ]
 })
 export class AppModule {}
